a4846508f4d35e2789b69be041fc1d0bf0cfaaf4
ramiyones <ramiyonelab@gmail.com>
2025-12-19 07:54:15 +0000

    chore(debug): temporarily disable App import and render test div in index.tsx
    Co-authored-by: Genie <genie@cosine.sh>

--- index.tsx
+++ index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
+// Temporarily disable full app to debug black screen
+// import App from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div style={{ color: 'white', padding: 40, fontSize: 24 }}>
+    TEST RENDER FROM INDEX.TSX
+  </div>
 );
